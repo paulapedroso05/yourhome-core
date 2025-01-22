cde390c3b779ae8172af5e8cdc02b383cf6947db
paulapedroso05 <paahpedroso05@gmail.com>
2025-01-22 20:42:08 -0300

    Add Corretor

---
+++ src/main/java/br/com/yourhome/core/corretor/Corretor.java
@@ -0,0 +1,30 @@
+package br.com.yourhome.core.corretor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Getter
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Corretor implements Serializable {
+
+    @Id
+    private String documento;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String creci;
+
+    private String bio;
+
+}
